602668fe066b669a11136c8fdc7069fd65832cfb
avriFromHome <yoti>
2026-09-01 19:06:12 +0300

    Refactor video conversion script to improve readability and maintainability

--- vid/compress_and_keep_date.bat
+++ vid/compress_and_keep_date.bat
@@ -5,11 +5,24 @@ set "outputDir=.\outpot_videos"
 
 if not exist "%outputDir%" mkdir "%outputDir%"
 
-for %%f in (".\canon_videos\*.MOV") do (
-    echo Converting file : "%%f"
-    ffmpeg -loglevel panic -y -i "%%f" -vcodec libx264 -crf 23 -preset medium -acodec aac -b:a 192k "%outputDir%\%%~nf.mp4"
+for %%f in (".\canon_videos*.MP4") do (
+echo Converting file: "%%f"
+
+```
+ffmpeg -loglevel panic -y -i "%%f" -vcodec libx264 -crf 23 -preset medium -acodec aac -b:a 192k "%outputDir%\%%~nf.mp4"
+
+if errorlevel 1 (
+    echo ERROR: Conversion failed for "%%f"
+) else (
     exiftool -tagsFromFile "%%f" -all:all "%outputDir%\%%~nf.mp4"
+
     del "%outputDir%\%%~nf.mp4_original"
+
+    echo Conversion successful. Deleting original: "%%f"
+    del "%%f"
+)
+```
+
 )
 
 echo Script complete!
